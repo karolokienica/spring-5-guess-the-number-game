f63c0c076bcf5bb904bcfeec2df5f789f74e524f
karolokienica1 <karol.okienica1@gmail.com>
2020-05-18 15:35:19 +0200

    applied thymeleaf

---
+++ web/src/main/java/okienica/karol/util/ViewNames.java
@@ -0,0 +1,10 @@
+package okienica.karol.util;
+
+public final class ViewNames {
+
+//    == constants ==
+    public static final String HOME = "home";
+
+//    == contructors ==
+    private ViewNames(){}
+}
