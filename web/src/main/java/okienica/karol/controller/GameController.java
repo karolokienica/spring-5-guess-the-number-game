7ee87f07d1ee0bdefadca7519fd1ef1df876e801
karolokienica1 <karol.okienica1@gmail.com>
2020-05-19 10:51:49 +0200

    added thymeleaf template for play view and controller

---
+++ web/src/main/java/okienica/karol/controller/GameController.java
@@ -0,0 +1,48 @@
+package okienica.karol.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import okienica.karol.service.GameService;
+import okienica.karol.util.AttributeNames;
+import okienica.karol.util.GameMappings;
+import okienica.karol.util.ViewNames;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Slf4j
+@Controller
+public class GameController {
+
+//    == fields ==
+    private final GameService gameService;
+
+//    == constructors ==
+    @Autowired
+    public GameController(GameService gameService) {
+        this.gameService = gameService;
+    }
+
+//    == request methods ==
+    @GetMapping(GameMappings.PLAY)
+    public String play(Model model){
+        model.addAttribute(AttributeNames.MAIN_MESSAAGE, gameService.getMainMessage());
+        model.addAttribute(AttributeNames.RESULT_MESSAGE, gameService.getResultMessage());
+        log.info("model = {}", model);
+
+        if(gameService.isGameOver()){
+            return ViewNames.GAME_OVER;
+        }
+
+        return ViewNames.PLAY;
+    }
+
+    @PostMapping(GameMappings.PLAY)
+    public String processMessage(@RequestParam int guess){
+        log.info("guess = {}", guess);
+        gameService.checkGuess(guess);
+        return GameMappings.REDIRECT_PLAY;
+    }
+}
---
+++ web/src/main/java/okienica/karol/util/AttributeNames.java
@@ -0,0 +1,11 @@
+package okienica.karol.util;
+
+public final class AttributeNames {
+
+//    == constants ==
+    public static final String MAIN_MESSAAGE = "mainMessage";
+    public static final String RESULT_MESSAGE = "resultMessage";
+
+//    == constructors ==
+    private AttributeNames() {}
+}
