0b01332041e8244279162a036abcc0fa2f36f76f
karolokienica1 <karol.okienica1@gmail.com>
2020-05-26 08:57:42 +0200

    added locale interceptor, finished the app

--- web/src/main/java/okienica/karol/config/WebConfig.java
+++ web/src/main/java/okienica/karol/config/WebConfig.java
@@ -2,13 +2,23 @@ package okienica.karol.config;
 
 import okienica.karol.interceptor.RequestInterceptor;
 import okienica.karol.util.ViewNames;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+//    == bean methods ==
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new SessionLocaleResolver();
+    }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
@@ -18,5 +28,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new RequestInterceptor());
+
+
+//        LocaleChangeInterceptor localeChangeInterceptor =
+//                new LocaleChangeInterceptor();
+//        localeChangeInterceptor.setParamName("lang"); // http://.../?lang=es
+        registry.addInterceptor(new LocaleChangeInterceptor()); // http://.../?locale=es
     }
 }
